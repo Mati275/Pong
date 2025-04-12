5673e864698b60e42b25080c24bf0b4afc3818f4
Mati275 <matitrepat@gmail.com>
2025-04-13 00:11:24 +0200

    .

--- RigidBody2D.gd
+++ RigidBody2D.gd
@@ -1,13 +1,20 @@
 extends RigidBody2D
 
+var impulse : Vector2 = Vector2(2, 0)
 
 func _ready():
 	linear_velocity = Vector2(-300, 0)
 
 func _integrate_forces(state):
 	print(state.get_contact_collider_object(1))
-	#for i in get_contact_count()
-		#
+	print(state.get_contact_count())
+	for i in state.get_contact_count():
+		var object = state.get_contact_collider_object(i)
+		if object is CharacterBody2D:
+			if linear_velocity.x > 0:
+				linear_velocity.x == (linear_velocity.x + 20) * -1
+			if linear_velocity.x < 0:
+				linear_velocity.x == (linear_velocity.x + 20) * -1
 
 #func _on_body_exited(body):
 	#var impulse : Vector2 = Vector2(100, 0)
